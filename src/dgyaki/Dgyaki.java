3efab60a22f4d9e07ea57e3fc478f6fe1aff89b7
SzaboErik <erikpapoka@gmail.com>
2022-04-21 10:58:56 +0200

    metódus létrehozása

---
+++ src/dgyaki/Dgyaki.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dgyaki;
+
+/**
+ *
+ * @author Erik
+ */
+public class Dgyaki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Dgyaki gbh = new Dgyaki();
+        gbh.metodus();
+        //ez
+    }
+    void metodus(){
+        System.out.println("egy metódus");
+    }
+    
+}
